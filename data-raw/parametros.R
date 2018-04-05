e4ea1ff36500f9748683d5ff9edc8c49f9d31757
diego <diegovalle@gmail.com>
2018-04-04 22:42:55 -0500

    parameters

--- data-raw/parametros.R
+++ data-raw/parametros.R
@@ -1,17 +1,19 @@
 library(httr)
 library(jsonlite)
 
-url = "http://sinaica.inecc.gob.mx/lib/j/php/getData.php"
+url <- "http://sinaica.inecc.gob.mx/lib/j/php/getData.php"
 fd <- list(
-  tabla  = "Datos",
-  fields = "DISTINCT parametro"
+  tabla   = "Parametros"
 )
 result <- POST(url,
                body = fd,
                encode = "form")
-json_result <- content(result, "text", encoding = "UTF-8")
-parameters <- fromJSON(json_result)
-names(parameters) <- "parameter"
+json_text <- content(result, "text", encoding = "UTF-8")
+parameters <- fromJSON(json_text)
+parameters$descripcion <- NULL
+parameters$tipoParametro <- NULL
+names(parameters) <- c("parameter_code", "parameter_name")
+Encoding(parameters$parameter_name) <- "UTF-8"
 
 write.csv(parameters, "data-raw/parameters.csv", row.names = FALSE)
 devtools::use_data(parameters, overwrite = TRUE)
