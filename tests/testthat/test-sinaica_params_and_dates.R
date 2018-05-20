b6af8fe37daa2c6a131d4609ab233972231a33c4
diego <diegovalle@gmail.com>
2018-05-20 08:22:52 -0500

    test errors in sinaica_station_dates()

--- tests/testthat/test-sinaica_params_and_dates.R
+++ tests/testthat/test-sinaica_params_and_dates.R
@@ -24,6 +24,10 @@ test_that("get-parameters_and_dates", {
 
 test_that("sinaica_station_dates", {
   skip_on_cran()
+
+  expect_error(sinaica_station_date())
+  expect_error(sinaica_station_date("ERROR"))
+
   expect_equal(sinaica_station_dates(271, "Manual"),
                c("1997-01-02", "2015-12-26"))
   expect_equal(sinaica_station_dates(42, "Manual"), c(NA, NA))
